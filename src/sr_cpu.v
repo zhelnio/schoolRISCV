321f1bc88a526c681f8b283a4c1671545676e24d
Stanislav Zhelnio <zhelniosl@yandex.ru>
2020-06-24 15:43:39 +0300

    regfile returned from schoolMIPS source

--- src/sr_cpu.v
+++ src/sr_cpu.v
@@ -223,3 +223,26 @@ module sr_alu
 
     assign zero   = (result == 0);
 endmodule
+
+module sm_register_file
+(
+    input         clk,
+    input  [ 4:0] a0,
+    input  [ 4:0] a1,
+    input  [ 4:0] a2,
+    input  [ 4:0] a3,
+    output [31:0] rd0,
+    output [31:0] rd1,
+    output [31:0] rd2,
+    input  [31:0] wd3,
+    input         we3
+);
+    reg [31:0] rf [31:0];
+
+    assign rd0 = (a0 != 0) ? rf [a0] : 32'b0;
+    assign rd1 = (a1 != 0) ? rf [a1] : 32'b0;
+    assign rd2 = (a2 != 0) ? rf [a2] : 32'b0;
+
+    always @ (posedge clk)
+        if(we3) rf [a3] <= wd3;
+endmodule
