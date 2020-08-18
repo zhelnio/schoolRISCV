368c29c96e234276a937da0f4f14f7148186b099
Stanislav Zhelnio <zhelniosl@yandex.ru>
2020-08-18 18:36:02 +0300

    byte2int awk converter replaced with the python one

---
+++ program/common/hex32.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import fileinput
+import argparse
+
+def line_process(line, out):
+    line = line.lower()
+
+    if line.startswith("@"):
+        out.write(line)
+        return
+
+    line = line.strip()
+    data = line.split(" ")
+
+    if len(data) % 4 != 0:
+        print("Error: input byte count is not multiple of 4\n: %s" % line)
+        exit(-1)
+
+    i = 0
+    while i < len(data):
+        out.write("%s%s%s%s\n" % (data[i+3], data[i+2], data[i+1], data[i]))
+        i += 4
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="input file path")
+parser.add_argument("output", help="output file path")
+args = parser.parse_args()
+
+with open(args.input,"r") as inp:
+    with open(args.output, "a") as out:
+        line = inp.readline()
+        while line:
+            line_process(line, out)
+            line = inp.readline()
